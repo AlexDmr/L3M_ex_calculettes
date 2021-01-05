486592b0e3f17c4a2daa94c741f185d5219a2fbc
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-01-05 19:11:55 +0100

    instruction simple

--- calculetteSimple.ts
+++ calculetteSimple.ts
@@ -3,7 +3,7 @@ export function computeSimple(str: string): number {
    * Utiliser split et trim pour produire les tableaux
    *   - des opérateurs
    *   - des opérandes
-   * Faites un reduce ensuite sur le tableau des opérandes
+   * Faites un reduce ensuite sur le tableau des operateurs
    * pour produire le résultat
    */
   return 0;
