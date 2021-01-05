d39fb159e01e3124cf4260822da3f510428d9458
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-01-05 18:35:34 +0100

    up

---
+++ calculetteSimple.ts
@@ -0,0 +1,10 @@
+export function computeSimple(str: string): number {
+  /*
+   * Utiliser split et trim pour produire les tableaux
+   *   - des opérateurs
+   *   - des opérandes
+   * Faites un reduce ensuite sur le tableau des opérandes
+   * pour produire le résultat
+   */
+  return 0;
+}
